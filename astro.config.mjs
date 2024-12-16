1e5b9f5f7b02e8804c40d26cd753dadb0262f92a
pixel-jellyfish <g3npixel@gmail.com>
2024-12-17 04:44:34 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pixeljellyfish.dev',
-  base: '/pixeljellyfish.dev',
+  site: 'https://code.pixeljellyfish.xyz',
+  base: '/code.pixeljellyfish.xyz',
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
